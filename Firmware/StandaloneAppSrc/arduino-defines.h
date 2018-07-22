d78af5272caa8f1177418430a116e5cd6cd17133
DarkCaster <dark.caster@outlook.com>
2018-07-22 04:12:23 +0300

    Firmware: added test-application sources

---
+++ Firmware/StandaloneAppSrc/arduino-defines.h
@@ -0,0 +1,11 @@
+#ifndef ARDUINO_DEFINES
+#define ARDUINO_DEFINES
+
+#include <chrono>
+
+#define PROGMEM
+#define pgm_read_byte(address) (*(address))
+#define millis() ((unsigned long)(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch())).count())
+
+
+#endif
---
+++ Firmware/StandaloneAppSrc/main.cpp
@@ -0,0 +1,58 @@
+#include "standalone_config.h"
+#include "main_loop.h"
+
+#if BUILD_PLATFORM==WINDOWS
+
+#include <windows.h>
+#include <tchar.h>
+#include <iostream>
+#include <signal.h>
+#include <cstdio>
+#include <chrono>
+#include <thread>
+
+static volatile boolean hangup=false;
+
+BOOL WINAPI ConsoleHandler(DWORD CEvent)
+{
+  switch(CEvent)
+  {
+    case CTRL_C_EVENT:
+    case CTRL_BREAK_EVENT:
+    case CTRL_CLOSE_EVENT:
+    case CTRL_LOGOFF_EVENT:
+    case CTRL_SHUTDOWN_EVENT:
+      hangup=true;
+      printf("Hangup...\n");
+      break;
+    default:
+      break;
+  }
+  return TRUE;
+}
+
+#endif //BUILD_PLATFORM==WINDOWS
+
+int main()
+{
+#if BUILD_PLATFORM==WINDOWS
+  if (SetConsoleCtrlHandler((PHANDLER_ROUTINE)ConsoleHandler,TRUE) == FALSE)
+  {
+    printf("Unable to install handler!\n");
+    return -1;
+  }
+  printf("Running setup\n");
+  setup();
+  printf("Starting main loop\n");
+  fflush(stdout);
+  while(!hangup)
+  {
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    loop();
+  }
+  printf("Exit\n");
+#else //BUILD_PLATFORM==WINDOWS
+#error This platform is not supported for now
+#endif
+  return 0;
+}
