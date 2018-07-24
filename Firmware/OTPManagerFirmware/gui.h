43c66a9ccd1baa09938c4df79f989e909b69ca63
DarkCaster <dark.caster@outlook.com>
2018-07-24 03:51:00 +0300

    Firmware: gui header-draft updated

--- Firmware/OTPManagerFirmware/gui.h
+++ Firmware/OTPManagerFirmware/gui.h
@@ -7,10 +7,42 @@
 #include <Arduino.h>
 #endif
 
+enum class MenuItemType : uint8_t
+{
+    MainScreen,
+    MainMenu,
+    ProfileMenu,
+    ProfileItem,
+    SettingsMenu,
+    SettingsItem,
+};
+
+struct MenuItem
+{
+    MenuItemType itemType;
+    int16_t itemIndex; //<0 - no item was selected
+};
+
 class GuiU8G2
 {
   public:
     void Init(uint8_t addr=0x78);
+    void Powersave(uint8_t mode);
+    void ResetToMainScr();
+    void ShowCDScr();
+    void ShowCEScr();
+    void ShowCodeScr(const char * const code);
+
+    //menu navigation methods, show, navigate and select menu items
+
+    //show menu, move to the next menu item
+    void MenuNext();
+    //go to the parent submenu
+    void MenuUp();
+    //advance submenu, select menu item. Return item description that was selected
+    MenuItem MenuSelect();
+
+    //TODO: reload and fillup menu
 };
 
 #endif
--- Firmware/StandaloneAppSrc/gui.cpp
+++ Firmware/StandaloneAppSrc/gui.cpp
@@ -5,3 +5,19 @@ void GuiU8G2::Init(uint8_t addr)
 {
   LOG("GuiU8G2::Init: address 0x%02x", addr);
 }
+
+
+void GuiU8G2::ResetToMainScr()
+{
+  LOG("GuiU8G2::ResetToMainScr: main screen");
+}
+
+void GuiU8G2::ShowCDScr()
+{
+  LOG("GuiU8G2::ShowCDScr: connecting...");
+}
+
+void GuiU8G2::ShowCEScr()
+{
+  LOG("GuiU8G2::ShowCEScr: connected!");
+}
