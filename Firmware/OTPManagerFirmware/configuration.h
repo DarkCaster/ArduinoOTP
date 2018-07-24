8eaea26a16f22b7e41ea87a18250865856ece271
DarkCaster <dark.caster@outlook.com>
2018-07-25 02:17:17 +0300

    Firmware: update gui-draft class, add MAIN_SCREEN_FONT define to configuration.h

--- Firmware/OTPManagerFirmware/configuration.h
+++ Firmware/OTPManagerFirmware/configuration.h
@@ -6,5 +6,6 @@
 #define RTC_POWER_PIN 4
 #define LED_SYNC LED_BUILTIN
 #define DISPLAY_ADDR 0x78
+#define MAIN_SCREEN_FONT u8g2_font_profont22_mn
 
 #endif
--- Firmware/OTPManagerFirmware/gui.cpp
+++ Firmware/OTPManagerFirmware/gui.cpp
@@ -1,6 +1,7 @@
 #include <U8g2lib.h>
 #include <U8x8lib.h>
 #include "gui.h"
+#include "configuration.h"
 
 static U8G2_SSD1306_128X64_NONAME_2_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
@@ -8,12 +9,6 @@ void GuiU8G2::Init(uint8_t addr)
 {
   u8g2.setI2CAddress(addr);
   u8g2.begin();
-  //remove remaining code
-  u8g2.firstPage();
-  do {
-    u8g2.setFont(u8g2_font_crox2cb_tf);
-    u8g2.drawStr(0,24,"Hello World!");
-  } while ( u8g2.nextPage() );
 }
 
 //TODO:
@@ -33,5 +28,19 @@ void GuiU8G2::ShowCEScr()
   u8g2.setFont(u8g2_font_crox2cb_tf);
   do {
     u8g2.drawStr(0,24,"< Connected >");
+  } while ( u8g2.nextPage() );
+}
+
+void GuiU8G2::Powersave(uint8_t mode)
+{
+  u8g2.setPowerSave(mode);
+}
+
+void GuiU8G2::ResetToMainScr()
+{
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(MAIN_SCREEN_FONT);
+    u8g2.drawStr(0,24,"0123456789:");
   } while ( u8g2.nextPage() );
 }
