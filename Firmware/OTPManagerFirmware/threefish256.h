1327995d6eaeb086c9b6b980516d1beadbe62d56
DarkCaster <dark.caster@outlook.com>
2018-07-28 22:23:46 +0300

    Firmware: threefish256 class updated

--- Firmware/OTPManagerFirmware/threefish256.h
+++ Firmware/OTPManagerFirmware/threefish256.h
@@ -10,8 +10,12 @@ class Threefish256
   private:
     static const uint64_t KeyScheduleConst = 0x1BD11BDAA9FC1A22;
     static const uint8_t ExpandedTweakSize = 3;
-    uint64_t ExpandedTweak[ExpandedTweakSize];
-    uint64_t ExpandedKey[ExpandedTweakSize];
+    uint64_t ExpandedTweak[ExpandedTweakSize]{};
+
+#define CipherSize 256
+#define CipherQwords (CipherSize / 64)
+    static const int ExpandedKeySize = CipherQwords + 1;
+    uint64_t ExpandedKey[ExpandedKeySize]{};
 
     static uint64_t RotateLeft64(uint64_t v, int8_t b);
     static uint64_t RotateRight64(uint64_t v, int8_t b);
@@ -19,11 +23,12 @@ class Threefish256
     static void Mix(uint64_t &a, uint64_t &b, int8_t r, uint64_t k0, uint64_t k1);
     static void UnMix(uint64_t &a, uint64_t &b, int8_t r);
     static void UnMix(uint64_t &a, uint64_t &b, int8_t r, uint64_t k0, uint64_t k1);
-
+  public:
+    Threefish256();
+    void Encrypt(const uint64_t * const input, uint64_t * const output);
+    void Decrypt(const uint64_t * const input, uint64_t * const output);
     void SetTweak(const uint64_t * const tweak);
     void SetKey(const uint64_t * const key);
-
-
 };
 
 #endif
