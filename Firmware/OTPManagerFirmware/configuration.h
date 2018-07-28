22f49f2ce2b16deb87d2508ff8a694eaf0d2af90
DarkCaster <dark.caster@outlook.com>
2018-07-28 11:54:36 +0300

    Firmware: configuration.h updated

--- Firmware/OTPManagerFirmware/configuration.h
+++ Firmware/OTPManagerFirmware/configuration.h
@@ -2,10 +2,14 @@
 #define CONFIGURATION_H
 
 #define SERIAL_PORT Serial
+#define SERIAL_PORT_SPEED 38400
 #define SERIAL_RX_PIN 0
 #define RTC_POWER_PIN 4
 #define LED_SYNC LED_BUILTIN
 #define DISPLAY_ADDR 0x78
 #define MAIN_SCREEN_FONT u8g2_font_profont22_mn
+
+#define DEBUG_SERIAL_PORT SERIAL_PORT
+#define DEBUG_OUT_BUFFER_SIZE 128
 
 #endif
