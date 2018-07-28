2ce3fbff07cb4c3a97eae2910e5fb3ead05d608a
DarkCaster <dark.caster@outlook.com>
2018-07-29 00:52:33 +0300

    Firmware: CipherBase class updated

--- Firmware/OTPManagerFirmware/cipher.h
+++ Firmware/OTPManagerFirmware/cipher.h
@@ -9,8 +9,8 @@ class CipherBase
     virtual uint8_t GetKeySize() = 0;
     virtual uint8_t GetTweakSize() = 0;
     virtual uint8_t GetBlockSize() = 0;
-    virtual void SetKey(const uint8_t * const NewKey) = 0;
-    virtual void SetTweak(const uint8_t * const NewTweak) = 0;
+    virtual void SetKey(const uint8_t * const newKey) = 0;
+    virtual void SetTweak(const uint8_t * const newTweak) = 0;
     virtual void EncryptBlock(const uint8_t * const inputBlock, uint8_t * const outputBlock) = 0;
     virtual void DecryptBlock(const uint8_t * const inputBlock, uint8_t * const outputBlock) = 0;
 };
