04167a07e350e3ff5f9532bfd0c3f60187707d22
DarkCaster <dark.caster@outlook.com>
2018-11-12 05:15:27 +0300

    OTPManager: added some device-connection and data-transfer abstracts and class-drafts to api-library

---
+++ OTPManager/OTPManagerApi/Helpers/Answer.cs
@@ -0,0 +1,49 @@
+﻿//
+// Request.cs
+//
+// Author:
+//       DarkCaster <dark.caster@outlook.com>
+//
+// Copyright (c) 2018
+//
+// MIT License
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+using System;
+namespace OTPManagerApi.Helpers
+{
+	public struct Answer
+	{
+		private const int CMD_MAX_PLSZ = 14;
+
+		public readonly AnsType ansType;
+		public readonly byte[] payload;
+		public readonly int plLen;
+
+		public Answer(AnsType ansType, byte[] plBuff, int offset, int length)
+		{
+			this.ansType = ansType;
+			this.payload = new byte[length];
+			Buffer.BlockCopy(plBuff, offset, this.payload, 0, length);
+			this.plLen = length;
+		}
+
+		public static Answer Invalid() => new Answer(AnsType.Invalid, new byte[0], 0, 0);
+	}
+}
