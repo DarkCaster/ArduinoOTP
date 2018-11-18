b13f03cc1a624a80f209a55e88f6788928597a75
DarkCaster <dark.caster@outlook.com>
2018-11-18 04:59:14 +0300

    Firmware: added LCGen helper-class

---
+++ Firmware/OTPManagerFirmware/lcgen.h
@@ -0,0 +1,19 @@
+#ifndef LCGEN
+#define LCGEN
+
+#include <Arduino.h>
+
+class LCGen
+{
+  private:
+    //following constants ensures that LGC overflow will occur in 4294967290 rounds (this gives us 2^32-5-1 uinique values)
+    //see http://statmath.wu-wien.ac.at/software/prng/doc/prng.html for constant selection details
+    const uint64_t m = 4294967291UL;
+    const uint64_t a = 279470273UL;
+    uint64_t curLCGValue;
+  public:
+    LCGen(const uint8_t * const seed);
+    uint32_t GenerateValue();
+};
+
+#endif
