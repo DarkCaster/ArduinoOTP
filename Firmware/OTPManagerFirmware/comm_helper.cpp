4b17aaef4a51930cd695cdd8433a4b8eb1b109b7
DarkCaster <dark.caster@outlook.com>
2018-11-19 01:40:08 +0300

    Firmware: comm_helper.cpp reformated, minor cast fix at CommHelper::ReceiveRequest method

--- Firmware/OTPManagerFirmware/comm_helper.cpp
+++ Firmware/OTPManagerFirmware/comm_helper.cpp
@@ -5,7 +5,7 @@
 
 Request::Request()
 {
-  plLen=0;
+	plLen=0;
 }
 
 Request::Request(const uint8_t req, const uint32_t seq, const uint8_t* const payload, const uint8_t plLen)
@@ -36,85 +36,85 @@ CommHelper::CommHelper(HardwareSerial * const port) : serial(port) { }
 
 void CommHelper::Init(const long speed)
 {
-  serial->begin(speed);
-  serial->setTimeout(CMD_TIMEOUT);
+	serial->begin(speed);
+	serial->setTimeout(CMD_TIMEOUT);
 }
 
 void CommHelper::Deinit()
 {
-  serial->end();
+	serial->end();
 }
 
 uint8_t CommHelper::DataAvailable()
 {
-  return serial->available()?1:0;
+	return serial->available()?1:0;
 }
 
 void CommHelper::FlushInput()
 {
-  while(serial->read()>-1){};
+	while(serial->read()>-1){};
 }
 
 Request CommHelper::ReceiveRequest()
 {
-  //message buffer
-  uint8_t recvBuff[CMD_BUFF_SIZE];
-  //read header
-  while(!serial->available()){}
-  serial->readBytes(recvBuff,1);
-  //verify header
-  auto remSz = static_cast<uint8_t>(*recvBuff & CMD_SIZE_MASK);
-  if(remSz<CMD_MIN_REMSZ||remSz>CMD_MAX_REMSZ)
-    return Request::Invalid();
-  //check header against supported commands list
-  auto req=(uint8_t)(*recvBuff & REQ_ALL_MASK);
-  switch(req)
-  {
-    case REQ_RESYNC:
-    case REQ_RESYNC_COMPLETE:
-    case REQ_PING:
-      break;
-    default:
-      return Request::Invalid();
-  }
-  //read message-body
-  auto startTime=millis();
-  auto rem=remSz;
-  while(rem>0)
-  {
-    rem-=static_cast<decltype(rem)>(serial->readBytes(recvBuff+CMD_HDR_SIZE+(remSz-rem),rem));
-    if(millis()-startTime>CMD_TIMEOUT)
-      return Request::Invalid();
-  }
-  //verify CRC
-  auto testSz=(uint8_t)(CMD_HDR_SIZE+remSz-1);
-  if(*(recvBuff+testSz)!=CRC8(recvBuff,testSz))
-    return Request::Invalid();
-  return Request(req,recvBuff+CMD_HDR_SIZE,remSz-CMD_CRC_SIZE);
+	//message buffer
+	uint8_t recvBuff[CMD_BUFF_SIZE];
+	//read header
+	while(!serial->available()){}
+	serial->readBytes(recvBuff,1);
+	//verify header
+	auto remSz = static_cast<uint8_t>(*recvBuff & CMD_SIZE_MASK);
+	if(remSz<CMD_MIN_REMSZ||remSz>CMD_MAX_REMSZ)
+		return Request::Invalid();
+	//check header against supported commands list
+	auto req=static_cast<uint8_t>(*recvBuff & REQ_ALL_MASK);
+	switch(req)
+	{
+		case REQ_RESYNC:
+		case REQ_RESYNC_COMPLETE:
+		case REQ_PING:
+			break;
+		default:
+			return Request::Invalid();
+	}
+	//read message-body
+	auto startTime=millis();
+	auto rem=remSz;
+	while(rem>0)
+	{
+		rem-=static_cast<decltype(rem)>(serial->readBytes(recvBuff+CMD_HDR_SIZE+(remSz-rem),rem));
+		if(millis()-startTime>CMD_TIMEOUT)
+			return Request::Invalid();
+	}
+	//verify CRC
+	auto testSz=(uint8_t)(CMD_HDR_SIZE+remSz-1);
+	if(*(recvBuff+testSz)!=CRC8(recvBuff,testSz))
+		return Request::Invalid();
+	return Request(req,recvBuff+CMD_HDR_SIZE,remSz-CMD_CRC_SIZE);
 }
 
 
 uint8_t CommHelper::SendAnswer(const AnsType answer, const uint8_t* const payload, uint8_t plLen)
 {
-  if(plLen>CMD_MAX_PLSZ)
-    return 0;
-  //message buffer
-  uint8_t cmdBuff[CMD_BUFF_SIZE];
-  //place data to cmdBuff, payload buffer may overlap with cmdBuff
-  for(uint8_t i=0; i<plLen; ++i)
-    *(cmdBuff+CMD_HDR_SIZE+i)=*(payload+i);
-  //write header
+	if(plLen>CMD_MAX_PLSZ)
+		return 0;
+	//message buffer
+	uint8_t cmdBuff[CMD_BUFF_SIZE];
+	//place data to cmdBuff, payload buffer may overlap with cmdBuff
+	for(uint8_t i=0; i<plLen; ++i)
+		*(cmdBuff+CMD_HDR_SIZE+i)=*(payload+i);
+	//write header
 #ifdef CMD_HDR_SIZE_IS_1
-  *cmdBuff=(uint8_t)(((uint8_t)answer)|(plLen+CMD_CRC_SIZE));
+	*cmdBuff=(uint8_t)(((uint8_t)answer)|(plLen+CMD_CRC_SIZE));
 #else
 #error unsupporned CMD_HDR_SIZE
 #endif
-  //write crc
-  auto testLen=(uint8_t)(plLen+CMD_HDR_SIZE);
-  *(cmdBuff+testLen)=CRC8(cmdBuff,testLen);
-  //send data
-  auto finalLen=(uint8_t)(testLen+CMD_CRC_SIZE);
-  for(uint8_t i=0; i<finalLen; ++i)
-    while(serial->write(*(cmdBuff+i))<1);
-  return 1;
+	//write crc
+	auto testLen=(uint8_t)(plLen+CMD_HDR_SIZE);
+	*(cmdBuff+testLen)=CRC8(cmdBuff,testLen);
+	//send data
+	auto finalLen=(uint8_t)(testLen+CMD_CRC_SIZE);
+	for(uint8_t i=0; i<finalLen; ++i)
+		while(serial->write(*(cmdBuff+i))<1);
+	return 1;
 }
