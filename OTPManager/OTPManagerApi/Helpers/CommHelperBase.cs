63e4e7b014c9b262561e21a3bada3c5a390ecc5c
DarkCaster <dark.caster@outlook.com>
2018-11-20 01:44:55 +0300

    OTPManager: CommHelperBase api-class updated

--- OTPManager/OTPManagerApi/Helpers/CommHelperBase.cs
+++ OTPManager/OTPManagerApi/Helpers/CommHelperBase.cs
@@ -27,6 +27,7 @@
 // SOFTWARE.
 
 using System;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 using OTPManagerApi.Protocol;
@@ -36,18 +37,18 @@ namespace OTPManagerApi.Helpers
 	public abstract class CommHelperBase
 	{
 		public abstract Task<Answer> ReceiveAnswer();
-		public abstract Task SendRequest(ReqType reqType, byte[] plBuff, int offset, int len);
+		public abstract Task SendRequest(ReqType reqType, uint seq, byte[] plBuff, int offset, int len);
 		public abstract int MaxPayloadSize { get; }
 
 		protected readonly ProtocolConfig config;
 
 		protected CommHelperBase(ProtocolConfig config) => this.config = config;
 
-		public async Task Resync()
+		public async Task<KeyValuePair<uint,uint>> Resync()
 		{
 			var reqBuff = new byte[MaxPayloadSize];
 			//send empty resync request
-			await SendRequest(ReqType.Resync, reqBuff, 0, 0);
+			await SendRequest(ReqType.Resync, 0, reqBuff, 0, 0);
 			//read and drop answers until timeout exception, or data limit was reached
 			var dataDropLeft = config.RESYNC_DATA_DROP_LIMIT;
 			while (dataDropLeft > 0)
@@ -65,11 +66,18 @@ namespace OTPManagerApi.Helpers
 			if (dataDropLeft <= 0)
 				throw new Exception("Cannot confirm resync request transmisson!");
 			//generate payload with random sequence
+			uint seq = 0;
 			using (var random = new RNGCryptoServiceProvider())
+			{
 				random.GetBytes(reqBuff);
-			//TODO: init send and receive LCGs
+				byte[] bSeq = new byte[4];
+				random.GetBytes(bSeq);
+				seq = unchecked((uint)(bSeq[0] | bSeq[1] << 8 | bSeq[2] << 16 | bSeq[3] << 24));
+			}
+			//init request LCG
+			var reqGen = new LCGen(seq);
 			//send resync sequence
-			await SendRequest(ReqType.Resync, reqBuff, 0, reqBuff.Length);
+			await SendRequest(ReqType.Resync, reqGen.GenerateValue(), reqBuff, 0, reqBuff.Length);
 			//receive resync answer
 			var verification = await ReceiveAnswer();
 			if (verification.ansType != AnsType.Resync)
@@ -77,14 +85,23 @@ namespace OTPManagerApi.Helpers
 			//compare resync sequence
 			if (verification.plLen != reqBuff.Length)
 				throw new Exception("Resync verification sequence is incorrect!");
+			//init answer LCG
+			var ansGen = new LCGen(verification.seq);
 			for (int i = 0; i < verification.plLen; ++i)
 				if (verification.payload[i] != reqBuff[i])
 					throw new Exception("Resync verification sequence mismatch!");
+			//get final request and answer LCG init values
+			var reqSeq = reqGen.GenerateValue();
+			var ansSeq = ansGen.GenerateValue();
 			//send resync-complete request
-			await SendRequest(ReqType.ResyncComplete, reqBuff, 0, 0);
+			await SendRequest(ReqType.ResyncComplete, reqSeq, reqBuff, 0, 0);
 			//receive OK answer
-			if ((await ReceiveAnswer()).ansType != AnsType.Ok)
+			var confirmation = await ReceiveAnswer();
+			if (confirmation.ansType != AnsType.Ok)
 				throw new Exception("Final resync-confirmation failed!");
+			if (confirmation.seq != ansSeq)
+				throw new Exception("Incorrect seqence number received in resync-confirmation!");
+			return new KeyValuePair<uint, uint>(reqSeq, ansSeq);
 		}
 	}
 }
