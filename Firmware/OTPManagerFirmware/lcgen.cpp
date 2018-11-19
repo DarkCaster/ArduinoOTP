4cdf9b61ff46b77212c1362e2dabef1636636c7f
DarkCaster <dark.caster@outlook.com>
2018-11-19 22:05:58 +0300

    Firmware: LCGen class updated

--- Firmware/OTPManagerFirmware/lcgen.cpp
+++ Firmware/OTPManagerFirmware/lcgen.cpp
@@ -1,17 +1,17 @@
 #include "lcgen.h"
 
-LCGen::LCGen(const uint8_t * const seed)
+LCGen::LCGen(const uint32_t seed)
 {
-    uint64_t init = static_cast<uint32_t>(seed[0]) | static_cast<uint32_t>(seed[1]) << 8 | static_cast<uint32_t>(seed[2]) << 16 | static_cast<uint32_t>(seed[3]) << 24;
-    if (init < 1)
-        init = 1;
-    if (init > (m - 1))
-        init = m - 1;
-    curLCGValue = init;
+	uint64_t init = seed;
+	if (init < 1)
+		init = 1;
+	if (init > (m - 1))
+		init = m - 1;
+	curLCGValue = init;
 }
 
 uint32_t LCGen::GenerateValue()
 {
-    curLCGValue = (curLCGValue * a) % m;
-    return static_cast<uint32_t>(curLCGValue);
+	curLCGValue = (curLCGValue * a) % m;
+	return static_cast<uint32_t>(curLCGValue);
 }
