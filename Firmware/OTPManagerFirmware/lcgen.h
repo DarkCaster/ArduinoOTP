e2ceafd32097387b69a17ef89e8c261271e9dbb3
DarkCaster <dark.caster@outlook.com>
2018-11-19 22:11:35 +0300

    Firmware: LCGen class updated

--- Firmware/OTPManagerFirmware/lcgen.h
+++ Firmware/OTPManagerFirmware/lcgen.h
@@ -8,8 +8,8 @@ class LCGen
 	private:
 		//following constants ensures that LGC overflow will occur in 4294967290 rounds (this gives us 2^32-5-1 uinique values)
 		//see http://statmath.wu-wien.ac.at/software/prng/doc/prng.html for constant selection details
-		const static uint64_t m = 4294967291UL;
-		const static uint64_t a = 279470273UL;
+		const static uint64_t m = 4294967291ULL;
+		const static uint64_t a = 279470273ULL;
 		uint64_t curLCGValue;
 	public:
 		LCGen(const uint32_t seed);
