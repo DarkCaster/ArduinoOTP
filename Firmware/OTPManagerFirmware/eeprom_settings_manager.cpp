1dea1390b122930a330a0c00cd18f15835450d6b
DarkCaster <dark.caster@outlook.com>
2018-12-05 16:13:17 +0300

    Firmware: EEPROMSettingsManager class updated

--- Firmware/OTPManagerFirmware/eeprom_settings_manager.cpp
+++ Firmware/OTPManagerFirmware/eeprom_settings_manager.cpp
@@ -1,12 +1,8 @@
 #include "eeprom_settings_manager.h"
 
-template<size_t KN, size_t EN> EEPROMSettingsManager::EEPROMSettingsManager(const uint8_t (&enc_key)[KN], uint8_t const (&enc_tweak)[EN]) :
-  key(enc_key),
-  tweak(enc_tweak)
-{ }
-
 void EEPROMSettingsManager::Commit()
 {
- //TODO
+
 }
+
 
--- Firmware/OTPManagerFirmware/eeprom_settings_manager.h
+++ Firmware/OTPManagerFirmware/eeprom_settings_manager.h
@@ -3,15 +3,24 @@
 
 #include <Arduino.h>
 #include "settings_manager.h"
+#include "cipher.h"
 
 class EEPROMSettingsManager final : public SettingsManager
 {
 	private:
-		const uint8_t (&key)[];
-		const uint8_t (&tweak)[];
+		const uint8_t * const key;
+		const uint8_t * const tweak;
+		const size_t keySz;
+		const size_t tweakSz;
+		const CipherBase &cipher;
 	public:
-		template<size_t KN, size_t EN> EEPROMSettingsManager(const uint8_t (&enc_key)[KN], uint8_t const (&enc_tweak)[EN]);
-		template<size_t KN, size_t EN> EEPROMSettingsManager(const uint8_t (&&enc_key)[KN], uint8_t const (&&enc_tweak)[EN]) = delete;
+		template<size_t KSZ, size_t TSZ> EEPROMSettingsManager(const uint8_t (&enc_key)[KSZ], uint8_t const (&enc_tweak)[TSZ], const CipherBase &cipher) :
+		  key(enc_key),
+		  tweak(enc_tweak),
+		  keySz(KSZ),
+		  tweakSz(TSZ),
+		  cipher(cipher) { }
+		template<size_t KSZ, size_t TSZ> EEPROMSettingsManager(const uint8_t (&&enc_key)[KSZ], uint8_t const (&&enc_tweak)[TSZ], const CipherBase &&cipher) = delete;
 		void Commit() final;
 };
 
