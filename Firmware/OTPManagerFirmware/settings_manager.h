3b4cf6ed87846b9bee25baa460b4ef62280ef294
DarkCaster <dark.caster@outlook.com>
2018-12-05 05:34:05 +0300

    Firmware: added SettingsManager abstract-class draft

---
+++ Firmware/OTPManagerFirmware/settings_manager.h
@@ -0,0 +1,20 @@
+#ifndef SETTINGS_MANAGER_H
+#define SETTINGS_MANAGER_H
+
+#include <Arduino.h>
+
+#pragma pack(push, settings, 1)
+struct Settings
+{
+	uint32_t utcOffset;
+};
+#pragma pack(pop, settings)
+
+class SettingsManager
+{
+	public:
+		Settings settings;
+		virtual void Commit() = 0;
+};
+
+#endif
