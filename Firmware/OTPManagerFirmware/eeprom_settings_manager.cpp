3eba7c94d7a397c1c2f3a75066cb4a206b1549b2
DarkCaster <dark.caster@outlook.com>
2018-12-06 19:16:31 +0300

    Firmware: EEPROMSettingsManager class updated, read settings at startup

--- Firmware/OTPManagerFirmware/eeprom_settings_manager.cpp
+++ Firmware/OTPManagerFirmware/eeprom_settings_manager.cpp
@@ -1,5 +1,6 @@
 #include "eeprom_settings_manager.h"
 #include "eeprom_writer.h"
+#include "eeprom_reader.h"
 #include "crc8.h"
 #include "debug.h"
 
@@ -41,17 +42,41 @@ void EEPROMSettingsManager::Commit()
 	//write settings block-by-block
 	auto blk=fullBlocks;
 	for(blk=0; blk<fullBlocks; ++blk)
-	{
 		if(!writer.WriteNextBlock(sPtr+blk*bsz))
 			FAIL(100,2000);
-	}
 	if(writer.WriteNextBlock(lastBlock))
 		FAIL(100,2000);
 }
 
 void EEPROMSettingsManager::Init()
 {
-	//TODO:
+	//get length of the settings structure
+	const auto sLen=sizeof(Settings);
+	if(!sLen)
+		return;
+	//calculate how much blocks (crc included) we need to read
+	auto bsz=cipher.GetBlockSize();
+	auto fullBlocks = (sLen+CRC_SZ) / bsz;
+	if((sLen+CRC_SZ) % bsz > 0)
+		fullBlocks++;
+	//create EEPROMReader
+	uint8_t tmpTweak[tweakSz];
+	memcpy(tmpTweak,tweak,tweakSz);
+	EEPROMReader reader(baseAddr,maxLen,cipher,key,tmpTweak);
+	//allocate space needed for temporary settings struct
+	uint8_t tmpBuff[fullBlocks*bsz];
+	//read data block-by-block
+	auto blk=fullBlocks;
+	for(blk=0; blk<fullBlocks; ++blk)
+		if(!reader.ReadNextBlock(tmpBuff+blk*bsz))
+			FAIL(100,2000);
+	//verify checksum
+	if(CRC8(tmpBuff,sLen)!=*(tmpBuff+fullBlocks*bsz-1))
+		return;
+	  //FAIL(100,3000); //for debug purposes
+	//if checksum is OK - save data to the settings struct
+	uint8_t *sPtr=reinterpret_cast<uint8_t*>(&settings);
+	memcpy(sPtr,tmpBuff,sLen);
 }
 
 
