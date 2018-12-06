1b1a1faff693f16855c44a0cd646d3f4dcadd6a6
DarkCaster <dark.caster@outlook.com>
2018-12-06 20:05:46 +0300

    Firmware: settings struct updated

--- Firmware/OTPManagerFirmware/settings.h
+++ Firmware/OTPManagerFirmware/settings.h
@@ -2,11 +2,13 @@
 #define SETTINGS_H
 
 #include <Arduino.h>
+#include "configuration.h"
 
 #pragma pack(push, settings, 1)
 struct Settings
 {
 	uint32_t utcOffset;
+	uint8_t  encTweak[ENCRYPTION_TWEAK_LEN];
 };
 #pragma pack(pop, settings)
 
