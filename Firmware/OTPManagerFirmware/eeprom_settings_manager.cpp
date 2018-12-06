75940d8d638ba46f24e95cbbe874d46e2302798f
DarkCaster <dark.caster@outlook.com>
2018-12-06 09:53:23 +0300

    Firmware: EEPROMSettingsManager class updated

--- Firmware/OTPManagerFirmware/eeprom_settings_manager.cpp
+++ Firmware/OTPManagerFirmware/eeprom_settings_manager.cpp
@@ -1,10 +1,58 @@
 #include "eeprom_settings_manager.h"
-#include <EEPROM.h>
+#include "eeprom_writer.h"
+#include "crc8.h"
+#include "debug.h"
+
+#define CRC_SZ 1
 
 void EEPROMSettingsManager::Commit()
 {
-	//get pointer (and length) to settings structure
-	//read and decrypt data block-by-block
+	//get pointer (and length) to the settings structure
+	const auto sLen=sizeof(Settings);
+	if(!sLen)
+		return;
+	uint8_t *sPtr=reinterpret_cast<uint8_t*>(&settings);
+	//calculate how much blocks we need to write
+	auto bsz=cipher.GetBlockSize();
+	auto fullBlocks = (sLen+CRC_SZ) / bsz;
+	auto remainingBytes = (sLen+CRC_SZ) % bsz;
+	//fillup last block and write checksum
+	uint8_t lastBlock[bsz];
+	if(!remainingBytes)
+	{
+		fullBlocks--;
+		memcpy(lastBlock, sPtr+fullBlocks*bsz, bsz-CRC_SZ);
+	}
+	else
+	{
+		memset(lastBlock, 0, bsz);
+		memcpy(lastBlock, sPtr+fullBlocks*bsz, remainingBytes);
+	}
+#if CRC_SZ == 1
+	//write crc
+	*(lastBlock+bsz-1)=CRC8(sPtr,sLen);
+#else
+#error TODO
+#endif
+	//create EEPROMWriter
+	uint8_t tmpTweak[tweakSz];
+	memcpy(tmpTweak,tweak,tweakSz);
+	EEPROMWriter writer(baseAddr,maxLen,cipher,key,tmpTweak);
+	//write settings block-by-block
+	auto blk=fullBlocks;
+#if DEBUG
+	for(blk=0; blk<fullBlocks; ++blk)
+	{
+		if(!writer.WriteNextBlock(sPtr+blk*bsz))
+			FAIL(100,2000);
+	}
+	if(writer.WriteNextBlock(lastBlock))
+		FAIL(100,2000);
+#else
+	for(blk=0; blk<fullBlocks; ++blk)
+		writer.WriteNextBlock(sPtr+blk*bsz);
+	writer.WriteNextBlock(lastBlock);
+#endif
 }
 
 void EEPROMSettingsManager::Init()
--- Firmware/OTPManagerFirmware/eeprom_settings_manager.h
+++ Firmware/OTPManagerFirmware/eeprom_settings_manager.h
@@ -13,13 +13,17 @@ class EEPROMSettingsManager final : public SettingsManager
 		const uint8_t * const tweak;
 		const size_t keySz;
 		const size_t tweakSz;
+		const int baseAddr;
+		const int maxLen;
 		CipherBase &cipher;
 	public:
-		template<size_t KSZ, size_t TSZ> EEPROMSettingsManager(const uint8_t (&enc_key)[KSZ], uint8_t const (&enc_tweak)[TSZ], CipherBase &cipher) :
+		template<size_t KSZ, size_t TSZ> EEPROMSettingsManager(const int baseAddr, const int maxLen, CipherBase &cipher, const uint8_t (&enc_key)[KSZ], uint8_t const (&enc_tweak)[TSZ]) :
 		  key(enc_key),
 		  tweak(enc_tweak),
 		  keySz(KSZ),
 		  tweakSz(TSZ),
+		  baseAddr(baseAddr),
+		  maxLen(maxLen),
 		  cipher(cipher)
 		{
 			if(this->cipher.GetKeySize()>KSZ)
@@ -27,7 +31,7 @@ class EEPROMSettingsManager final : public SettingsManager
 			if(this->cipher.GetTweakSize()>TSZ)
 				FAIL(100,500);
 		}
-		template<size_t KSZ, size_t TSZ> EEPROMSettingsManager(const uint8_t (&&)[KSZ], uint8_t const (&&)[TSZ], CipherBase &&) = delete;
+		template<size_t KSZ, size_t TSZ> EEPROMSettingsManager(const int baseAddr, const int maxLen, CipherBase &&, const uint8_t (&&)[KSZ], uint8_t const (&&)[TSZ]) = delete;
 		void Commit() final;
 		void Init() final;
 };
