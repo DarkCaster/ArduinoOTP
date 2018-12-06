a1cd7198bcea9a6d7851ebed3967655a87fa0a37
DarkCaster <dark.caster@outlook.com>
2018-12-06 07:33:19 +0300

    Firmware: added EEPROMWriter service class

---
+++ Firmware/OTPManagerFirmware/eeprom_writer.cpp
@@ -0,0 +1,41 @@
+#include "eeprom_writer.h"
+#include "debug.h"
+#include "EEPROM.h"
+
+EEPROMWriter::EEPROMWriter(const int baseAddr, const int maxLen, CipherBase& cipher, const uint8_t* const encKey, uint8_t* const tweak) :
+  curAddr(baseAddr),
+  limit(baseAddr+maxLen),
+  cipher(cipher),
+  encKey(encKey),
+  tweak(tweak)
+{
+	if(baseAddr<limit)
+		FAIL(100,500);
+	if(limit>static_cast<int>(EEPROM.length()))
+		FAIL(100,1000);
+}
+
+bool EEPROMWriter::WriteNextBlock(const uint8_t* const data)
+{
+	const uint8_t bsz=cipher.GetBlockSize();
+	const uint8_t tsz=cipher.GetTweakSize();
+	//create buffer for output data
+	uint8_t encData[bsz];
+	//encrypt block
+	cipher.EncryptBlock(data,encData,encKey,tweak);
+	//check, that we will not write outsize bounds
+	if(limit-curAddr<bsz)
+		return false;
+	//modify tweak array
+	uint8_t pos=0;
+	for(uint8_t tp=0; tp<tsz; ++tp)
+	{
+		*(tweak+tp)^=*(encData+pos++);
+		if(pos==bsz)
+			pos=0;
+	}
+	//write data to eeprom
+	for(uint8_t bp=0; bp<bsz; ++bp)
+		EEPROM.update(curAddr++,*(encData+bp));
+	return true;
+}
---
+++ Firmware/OTPManagerFirmware/eeprom_writer.h
@@ -0,0 +1,21 @@
+#ifndef EEPROM_WRITER_H
+#define EEPROM_WRITER_H
+
+#include <Arduino.h>
+#include "cipher.h"
+
+class EEPROMWriter
+{
+	private:
+		int curAddr;
+		const int limit;
+		CipherBase &cipher;
+		const uint8_t * const encKey;
+		uint8_t * const tweak;
+	public:
+		EEPROMWriter(const int baseAddr, const int maxLen, CipherBase &cipher, const uint8_t * const encKey, uint8_t * const tweak);
+		bool WriteNextBlock(const uint8_t * const data);
+		bool SkipNextBlock(const uint8_t * const data);
+};
+
+#endif
