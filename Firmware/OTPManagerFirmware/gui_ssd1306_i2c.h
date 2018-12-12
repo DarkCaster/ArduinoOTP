4512bbcd5b017077cd6af1cd3d0a766fe7d3b8d0
DarkCaster <dark.caster@outlook.com>
2018-12-12 21:58:41 +0300

    Firmware: GuiSSD1306_I2C class updated, reformatted

--- Firmware/OTPManagerFirmware/gui_ssd1306_i2c.h
+++ Firmware/OTPManagerFirmware/gui_ssd1306_i2c.h
@@ -9,31 +9,31 @@
 
 class GuiSSD1306_I2C final : GuiBase
 {
-  private:
-    const uint8_t displayPowerPin;
-    const uint8_t displayAddr;
+	private:
+		const uint8_t displayPowerPin;
+		const uint8_t displayAddr;
 		ClockHelperBase &clockHelper;
 		ProfileManager &profileManager;
 		LCGRandom rnd;
 
-  public:
+	public:
 		GuiSSD1306_I2C(uint8_t displayPowerPin, uint8_t displayAddr, ClockHelperBase &clockHelper, ProfileManager &profileManager);
 		GuiSSD1306_I2C(uint8_t displayPowerPin, uint8_t displayAddr, ClockHelperBase &&, ProfileManager &&) = delete;
-    void InitPre() final;
-    void InitPost() final;
-    void DescendPre() final;
-    void WakeupPre() final;
-    void DescendPost() final;
-    void WakeupPost() final;
-    MenuItem ResetToMainScr() final;
-    void ShowCDScr() final;
-    void ShowCEScr() final;
-    void ShowCodeScr(const char * const code) final;
+		void InitPre() final;
+		void InitPost() final;
+		void DescendPre() final;
+		void WakeupPre() final;
+		void DescendPost() final;
+		void WakeupPost() final;
+		MenuItem ResetToMainScr() final;
+		void ShowCDScr() final;
+		void ShowCEScr() final;
+		void ShowCodeScr(const char * const code) final;
 		void Reseed();
 
-    //menu navigation methods, show, navigate and select menu items
-    void MenuNext() final;
-    MenuItem MenuSelect() final;
+		//menu navigation methods, show, navigate and select menu items
+		void MenuNext() final;
+		MenuItem MenuSelect() final;
 };
 
 #endif
