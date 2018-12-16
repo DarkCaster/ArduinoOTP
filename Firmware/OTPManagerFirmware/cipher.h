80a97ac95c416b8f977f900e2fa48fa6852c5688
DarkCaster <dark.caster@outlook.com>
2018-12-16 11:43:17 +0300

    Firmware: CipherBase class updated, reformat

--- Firmware/OTPManagerFirmware/cipher.h
+++ Firmware/OTPManagerFirmware/cipher.h
@@ -6,10 +6,10 @@
 //NOTE: derived class should be immutable and threadsafe to support sharing it's instance between other objects
 class CipherBase
 {
-  public:
-    virtual uint8_t GetKeySize() = 0;
-    virtual uint8_t GetTweakSize() = 0;
-    virtual uint8_t GetBlockSize() = 0;
+	public:
+		virtual uint8_t GetKeySize() = 0;
+		virtual uint8_t GetTweakSize() = 0;
+		virtual uint8_t GetBlockSize() = 0;
 		virtual void EncryptBlock(const uint8_t * const inputBlock, uint8_t * const outputBlock, const uint8_t * const key, const uint8_t * const tweak) = 0;
 		virtual void DecryptBlock(const uint8_t * const inputBlock, uint8_t * const outputBlock, const uint8_t * const key, const uint8_t * const tweak) = 0;
 };
