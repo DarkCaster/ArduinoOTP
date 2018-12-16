94ed6c66a20bbca46393ee43b6d958f1f147a74a
DarkCaster <dark.caster@outlook.com>
2018-12-16 11:39:37 +0300

    Firmware: ClockHelperDS3231 class updated, reformat

--- Firmware/OTPManagerFirmware/clock_helper_DS3231.cpp
+++ Firmware/OTPManagerFirmware/clock_helper_DS3231.cpp
@@ -6,59 +6,59 @@ static const char daysOfTheWeek[MAIN_SCREEN_DOW_LEN][MAIN_SCREEN_DAY_LEN] PROGME
 
 static void WritePositiveNumber(char * const target, uint8_t &pos, const uint16_t number, const uint8_t zeros, const uint8_t signs)
 {
-   for(uint8_t i=0;i<zeros;++i)
-     *(target+pos++)='0';
-	 itoa(static_cast<int>(number),(target+pos),10);
-   pos+=signs-zeros;
+	for(uint8_t i=0;i<zeros;++i)
+		*(target+pos++)='0';
+	itoa(static_cast<int>(number),(target+pos),10);
+	pos+=signs-zeros;
 }
 
 static void Write2SignNumber(char * const target, uint8_t &pos, const uint8_t number)
 {
-  uint8_t zeros=number>9?0:1;
-  WritePositiveNumber(target, pos, number, zeros, 2);
+	uint8_t zeros=number>9?0:1;
+	WritePositiveNumber(target, pos, number, zeros, 2);
 }
 
 /*static void Write4SignNumber(char * const target, uint8_t &pos, const uint16_t number)
 {
-  uint8_t zeros=number>999?0:number>99?1:number>9?2:3;
-  WritePositiveNumber(target, pos, number, zeros, 4);
+	uint8_t zeros=number>999?0:number>99?1:number>9?2:3;
+	WritePositiveNumber(target, pos, number, zeros, 4);
 }*/
 
 void ClockHelperDS3231::WriteTimeString(char * const target, const uint8_t maxLen)
 {
-  if(maxLen<6)
-  {
-    *(target)=0;
-    return;
-  }
-  uint8_t pos=0;
-  //write hour
-  Write2SignNumber(target,pos,lastTime.Hour);
-  //write delim
-  *(target+pos++)=':';
-  //write hour
-  Write2SignNumber(target,pos,lastTime.Minute);
-  *(target+pos++)=0;
+	if(maxLen<6)
+	{
+		*(target)=0;
+		return;
+	}
+	uint8_t pos=0;
+	//write hour
+	Write2SignNumber(target,pos,lastTime.Hour);
+	//write delim
+	*(target+pos++)=':';
+	//write hour
+	Write2SignNumber(target,pos,lastTime.Minute);
+	*(target+pos++)=0;
 }
 
 void ClockHelperDS3231::WriteDateString(char * const target, const uint8_t maxLen)
 {
-  if(maxLen<32)
-  {
-    *(target)=0;
-    return;
-  }
-  uint8_t pos=0;
-  //write day of the week
-  for(uint8_t i=0;i<MAIN_SCREEN_DAY_LEN-1;++i)
-    *(target+pos++)=static_cast<char>(pgm_read_byte(*(daysOfTheWeek+lastTime.Dow)+i));
-  *(target+pos++)=' ';
-  Write2SignNumber(target,pos,lastTime.Day);
-  *(target+pos++)='/';
-  Write2SignNumber(target,pos,lastTime.Month);
-  *(target+pos++)='/';
-  Write2SignNumber(target,pos,lastTime.Year);
-  *(target+pos++)=0;
+	if(maxLen<32)
+	{
+		*(target)=0;
+		return;
+	}
+	uint8_t pos=0;
+	//write day of the week
+	for(uint8_t i=0;i<MAIN_SCREEN_DAY_LEN-1;++i)
+		*(target+pos++)=static_cast<char>(pgm_read_byte(*(daysOfTheWeek+lastTime.Dow)+i));
+	*(target+pos++)=' ';
+	Write2SignNumber(target,pos,lastTime.Day);
+	*(target+pos++)='/';
+	Write2SignNumber(target,pos,lastTime.Month);
+	*(target+pos++)='/';
+	Write2SignNumber(target,pos,lastTime.Year);
+	*(target+pos++)=0;
 }
 
 ClockHelperDS3231::ClockHelperDS3231(uint8_t _rtcPowerPin, SettingsManager &_settingsManager) :
@@ -69,31 +69,31 @@ ClockHelperDS3231::ClockHelperDS3231(uint8_t _rtcPowerPin, SettingsManager &_set
 
 void ClockHelperDS3231::InitPre()
 {
-  //power-on
-  pinMode(rtcPowerPin, OUTPUT);
-  digitalWrite(rtcPowerPin,HIGH);
+	//power-on
+	pinMode(rtcPowerPin, OUTPUT);
+	digitalWrite(rtcPowerPin,HIGH);
 }
 
 void ClockHelperDS3231::InitPost()
 {
-  clock.begin();
+	clock.begin();
 }
 
 void ClockHelperDS3231::WakeupPre()
 {
-  digitalWrite(rtcPowerPin,HIGH);
+	digitalWrite(rtcPowerPin,HIGH);
 }
 
 void ClockHelperDS3231::WakeupPost()
 {
-  clock.begin();
+	clock.begin();
 }
 
 void ClockHelperDS3231::DescendPre() { }
 
 void ClockHelperDS3231::DescendPost()
 {
-  digitalWrite(rtcPowerPin,LOW);
+	digitalWrite(rtcPowerPin,LOW);
 }
 
 void ClockHelperDS3231::Update()
--- Firmware/OTPManagerFirmware/clock_helper_DS3231.h
+++ Firmware/OTPManagerFirmware/clock_helper_DS3231.h
@@ -8,24 +8,24 @@
 
 class ClockHelperDS3231 final : public ClockHelperBase
 {
-  private:
-    const uint8_t rtcPowerPin;
+	private:
+		const uint8_t rtcPowerPin;
 		SettingsManager &settingsManager;
 		DS3231_Simple clock;
-    DateTime lastTime;
-  public:
+		DateTime lastTime;
+	public:
 		ClockHelperDS3231(uint8_t rtcPowerPin, SettingsManager &settingsManager);
 		ClockHelperDS3231(uint8_t rtcPowerPin, SettingsManager &&) = delete;
-    //TODO: getting time ticks for various OTP algorithms
-    void InitPre() final;
-    void InitPost() final;
-    void WriteTimeString(char * const target, const uint8_t maxLen) final;
-    void WriteDateString(char * const target, const uint8_t maxLen) final;
-    void WakeupPre() final;
-    void DescendPre() final;
-    void WakeupPost() final;
-    void DescendPost() final;
-    void Update() final;
+		//TODO: getting time ticks for various OTP algorithms
+		void InitPre() final;
+		void InitPost() final;
+		void WriteTimeString(char * const target, const uint8_t maxLen) final;
+		void WriteDateString(char * const target, const uint8_t maxLen) final;
+		void WakeupPre() final;
+		void DescendPre() final;
+		void WakeupPost() final;
+		void DescendPost() final;
+		void Update() final;
 		bool SetTime(const uint8_t &sec, const uint8_t &min, const uint8_t &hour, const uint8_t &day, const uint8_t &dow, const uint8_t &month, const uint16_t &year, const int32_t &utcOffset) final;
 		uint32_t GetSeed() final;
 };
