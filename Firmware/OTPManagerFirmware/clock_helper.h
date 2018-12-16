bba3a748f71642ce572e205f28b373b8d05ac790
DarkCaster <dark.caster@outlook.com>
2018-12-16 11:47:56 +0300

    Firmware: ClockHelperBase class updated, reformat

--- Firmware/OTPManagerFirmware/clock_helper.h
+++ Firmware/OTPManagerFirmware/clock_helper.h
@@ -6,11 +6,11 @@
 
 class ClockHelperBase : LowPowerDevice
 {
-  public:
-    //TODO: getting time ticks for various OTP algorithms
-    virtual void WriteTimeString(char * const target, const uint8_t maxLen) = 0;
-    virtual void WriteDateString(char * const target, const uint8_t maxLen) = 0;
-    virtual void Update() = 0;
+	public:
+		//TODO: getting time ticks for various OTP algorithms
+		virtual void WriteTimeString(char * const target, const uint8_t maxLen) = 0;
+		virtual void WriteDateString(char * const target, const uint8_t maxLen) = 0;
+		virtual void Update() = 0;
 		virtual bool SetTime(const uint8_t &sec, const uint8_t &min, const uint8_t &hour, const uint8_t &day, const uint8_t &dow, const uint8_t &month, const uint16_t &year, const int32_t &utcOffset) = 0;
 		virtual uint32_t GetSeed() = 0;
 };
