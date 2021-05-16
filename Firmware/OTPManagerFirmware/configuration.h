ce6ff3b041abe3b38647c0ded9caa64b750891fa
DarkCaster <dark.caster@outlook.com>
2021-05-17 02:56:27 +0300

    Firmware: configuration.h updated - PROFILE_PAYLOAD_LEN set to 32 bytes

--- Firmware/OTPManagerFirmware/configuration.h
+++ Firmware/OTPManagerFirmware/configuration.h
@@ -11,7 +11,7 @@
 #define EEPROM_SETTINGS_LEN 0x0040
 
 #define PROFILE_NAME_LEN 14 //+ 1 byte profile-type header, +1 byte CRC8 = 16 bytes
-#define PROFILE_PAYLOAD_LEN 15 // +1 byte CRC8 = 16 bytes
+#define PROFILE_PAYLOAD_LEN 31 // +1 byte CRC8 = 32 bytes
 
 #define EEPROM_PROFILE_STORAGE_ADDR 0x0040
 #define EEPROM_PROFILE_STORAGE_LEN 0x03C0
